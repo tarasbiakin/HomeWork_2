c3ee406f89a328220b82781378bf56e2f4219c28
tarasbiakin <115660593+tarasbiakin@users.noreply.github.com>
2022-11-15 19:49:15 +0300

    10

---
+++ 10/Program.cs
@@ -0,0 +1,15 @@
+﻿//  Напишите программу, которая принимает на вход трёхзначное число и 
+//  на выходе показывает вторую цифру этого числа. Выполнить с помощью 
+//  числовых операций (целочисленное деление, остаток от деления).
+
+Console.Write("Введите трехзначное число:");
+int sekondNum = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(KillSecondDigit(sekondNum));
+
+int KillSecondDigit(int num)
+{
+    int fitstStep = (num / 10)%10;
+    int result = fitstStep;
+    return result;
+}
