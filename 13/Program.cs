d3bdde72c8c2eec496c9c9582ff72c8a3dc7725a
tarasbiakin <115660593+tarasbiakin@users.noreply.github.com>
2022-11-16 11:41:43 +0300

    13

---
+++ 13/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+//  что третьей цифры нет. Выполнить с помощью числовых операций 
+//  (целочисленное деление, остаток от деления).
+
+Console.Write("Введите число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+if(num1>99){
+Console.WriteLine(KillDigit(num1));
+
+int KillDigit(int num)
+{
+    
+    int fitstStep = (num % 10);
+    int result = fitstStep;
+    return result;
+}
+
+
+}else
+Console.Write("Третьей цифры нет ");
