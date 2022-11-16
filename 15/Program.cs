d75eb2841b7acd2a809b27ba47e0d1e189a911c2
tarasbiakin <115660593+tarasbiakin@users.noreply.github.com>
2022-11-16 12:29:34 +0300

    15

--- 15/Program.cs
+++ 15/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Write("Введите число:");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if ((day<8)&&(day>0)){
+Console.WriteLine(square(day) ? "выходной" : "рабочий");
+
+bool square (int dau1)
+{
+    return (day<8)&&(day>5);}
+}
+else
+{
+    Console.WriteLine($"Такого дня нет");
+    
+}
