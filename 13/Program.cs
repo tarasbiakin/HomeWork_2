2541e21f8e0894d1cea15040478ca29e548641dd
tarasbiakin <115660593+tarasbiakin@users.noreply.github.com>
2022-11-16 11:58:04 +0300

    13

--- 13/Program.cs
+++ 13/Program.cs
@@ -10,12 +10,22 @@ Console.WriteLine(KillDigit(num1));
 
 int KillDigit(int num)
 {
-    
     int fitstStep = (num % 10);
     int result = fitstStep;
     return result;
+   }
+}
+else if(num1>999){
+Console.WriteLine(KillDigit(num1));
+
+int KillDigit(int num)
+{
+    int fitstStep = (num % 100);
+    int result = fitstStep;
+    return result;
+   }
 }
 
 
-}else
+else
 Console.Write("Третьей цифры нет ");
